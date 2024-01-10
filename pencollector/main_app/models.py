c30041144c487126e81a73fd82cfec9aeb3f3acf
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2024-01-10 12:36:50 -0500

    added pen models

--- pencollector/main_app/models.py
+++ pencollector/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pen(models.Model):
+    color = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    length = models.IntegerField()
